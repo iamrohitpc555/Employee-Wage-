49509da868a383cc63bb5c974326999a79dfb11b
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-04 13:03:35 +0530

    Update Employee_Wage_Builder.java

--- Employee_Wage_Builder.java
+++ Employee_Wage_Builder.java
@@ -1,34 +1,10 @@
-public class UC7EmpWageBuilderRefactorClass{
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int Emp_Rate_Per_Hr = 20;
-    public static final int Num_Of_Working_Days = 20;
-    public static final int Max_Hrs_In_Month = 100;
+public class welcome
+{
+    public static void main(String[] args)
+       {
 
-    public static int evaluateEmpWage() {
-        int empHrs = 0 , totalEmpHrs = 0 , totalWorkingDays = 0;
-        while (totalEmpHrs <= Max_Hrs_In_Month &&
-              totalWorkingDays < Num_Of_Working_Days){
-           totalWorkingDays++;
-           int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-           switch (empCheck){
-           case IS_PART_TIME:
-               empHrs = 4;
-               break;
-           case IS_FULL_TIME:
-               empHrs = 8;
-               break;
-           default:
-               empHrs = 0;
-        }
-        totalEmpHrs+= empHrs;
-        System.out.println("Day: " + totalWorkingDays +"  "+ "Employee working hours:" + empHrs );
-     }
-     int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hr;
-     System.out.println("Total Employee Wage is "+ totalEmpWage);
-     return totalEmpWage;
-   }
-   public static void main(String args[]){
-       evaluateEmpWage();
-   }
+           System.out.println("--------------------------------------------------------------------");
+           System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+           System.out.println("--------------------------------------------------------------------");
+    }
 }
