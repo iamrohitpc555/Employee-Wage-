55e7bcfc7ed1aa5ca5cc13f1202ca3a1e24e2bba
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-04 12:59:47 +0530

    Update Employee_Wage_Builder.java
    Added Company

--- Employee_Wage_Builder.java
+++ Employee_Wage_Builder.java
@@ -1,34 +1,71 @@
-public class UC7EmpWageBuilderRefactorClass{
+public class Emp_computation1
+{
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int Emp_Rate_Per_Hr = 20;
-    public static final int Num_Of_Working_Days = 20;
-    public static final int Max_Hrs_In_Month = 100;
+    
+    public String company;
+    int Emp_Rate_Per_Hr;
+    int Num_Of_Working_Days;
+    int Max_Hrs_In_Month;
+    
 
-    public static int evaluateEmpWage() {
-        int empHrs = 0 , totalEmpHrs = 0 , totalWorkingDays = 0;
-        while (totalEmpHrs <= Max_Hrs_In_Month &&
-              totalWorkingDays < Num_Of_Working_Days){
+    public Emp_computation1(String company,int Emp_Rate_Per_Hr ,int Num_Of_Working_Days, int Max_Hrs_In_Month)
+    {
+    		this.company=company;
+    		this.Emp_Rate_Per_Hr=Emp_Rate_Per_Hr;
+    		this.Num_Of_Working_Days=Num_Of_Working_Days;
+    		this.Max_Hrs_In_Month=Max_Hrs_In_Month;
+    }
+    
+    
+    public void evaluateEmpWage() 
+    {
+        int empHrs = 0;
+        int totalEmpWage=0;
+        int totalEmpHrs = 0; 
+        int totalWorkingDays = 0;
+        
+        
+        
+        while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days)
+        {
            totalWorkingDays++;
            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-           switch (empCheck){
-           case IS_PART_TIME:
-               empHrs = 4;
-               break;
-           case IS_FULL_TIME:
-               empHrs = 8;
-               break;
-           default:
-               empHrs = 0;
+          
+           switch (empCheck)
+           {
+           		case IS_PART_TIME:
+	               empHrs = 4;
+	               break;
+	           
+	           case IS_FULL_TIME:
+	               empHrs = 8;
+	               break;
+	           
+	           default:
+	               empHrs = 0;
+           }
+           totalEmpHrs+= empHrs;
         }
-        totalEmpHrs+= empHrs;
-        System.out.println("Day: " + totalWorkingDays +"  "+ "Employee working hours:" + empHrs );
-     }
-     int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hr;
-     System.out.println("Total Employee Wage is "+ totalEmpWage);
-     return totalEmpWage;
+        
+      totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hr;
+     System.out.println("Company: \"+company+\" Total Employee Wage: "+ totalEmpWage);
    }
-   public static void main(String args[]){
-       evaluateEmpWage();
+    
+    public static void main(String[] args)
+   {
+	   
+	   System.out.println("--------------------------------------------------------------------");
+       System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+       System.out.println("--------------------------------------------------------------------");
+       
+       Emp_computation1 Apple= new Emp_computation1("Apple" ,12,34,56);
+       Emp_computation1 Google=new Emp_computation1("Google", 14,44,16);
+       Emp_computation1 Microsoft=new Emp_computation1("Microsoft", 15,44,26);
+       
+       Apple.evaluateEmpWage();
+       Google.evaluateEmpWage();
+       Microsoft.evaluateEmpWage();
+       
    }
 }
